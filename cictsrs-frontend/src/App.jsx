57d25e6270c66b5164587ba3bcb377add90481b2
Ernesto III Dela Cruz <ernestoiiigdelacruz@gmail.com>
2026-04-08 16:14:01 +0800

    Added: Frontend files

--- cictsrs-frontend/src/App.jsx
+++ cictsrs-frontend/src/App.jsx
@@ -6,11 +6,12 @@ function App() {
   
 
   return (
+
       <Router>
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1> About</h1>} />
-          <Route path="/contact" element={<h1> Contact Us</h1>} />
+          <Route path="/" element={<h1> About</h1>} />
+          <Route path="/" element={<h1> Contact Us</h1>} />
 
 
         </Routes>
