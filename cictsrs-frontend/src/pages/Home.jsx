6dee4007fa72577fedba8982b8d784dedc6150c6
Ernesto III Dela Cruz <ernestoiiigdelacruz@gmail.com>
2026-04-22 10:21:55 +0800

    nav skeleton

--- cictsrs-frontend/src/pages/Home.jsx
+++ cictsrs-frontend/src/pages/Home.jsx
@@ -4,14 +4,57 @@ import '../App.css';
 const Home = () => {
     return (
 
-            <div className="flex flex-col  items-center justify-center h-screen ">
+<div class="grid grid-flow-col grid-rows-3 w-full h-screen "> 
+ 
+    <div class=" row-span-3 bg-gray-300 "> {/* Grid Sidebar container */}
+            <div class="flex flex-col items-center justify-center h-screen"> {/* FLex Nav Sidebar container */}
                 
-
             
-
+                
+                
+                
+                
             </div>
-   
+
+
+
+
+    
+        </div>
+  
+  
+    <div class="col-span-2  bg-gray-400 "> {/* Grid Navbar container */}
+         <div class="flex flex-col items-center justify-center h-screen"> {/* Flex Navbar container */} 
+
+
+
+
+
+
+
+        </div>
+        
+        
+        
+        
+    </div>
+     <div class="col-span-2 row-span-2  bg-gray-600">{/* Grid content area cintainer */}
+        <div class="flex flex-col items-center justify-center h-screen"> {/* Flex content area container */}
+
+    
+
+
+
+
+
+        </div>
+
+     </div>
+
+</div>
     );
+
+    
 
 }
 
--- cictsrs-frontend/src/pages/Login.jsx
+++ cictsrs-frontend/src/pages/Login.jsx
@@ -8,12 +8,7 @@ const Login = () => {
             <div className="flex flex-col  items-center justify-center h-screen">
                 
 
-                <div className=" bg-complementaryblue text-4xl px-5 py-10 font-bold mb-8 text-white rounded-lg">
-                    
-                        <button className="px-2 py-1 bg-primaryblue text-black  rounded-xl transition duration-500">
-                           LOGIN
-                        </button>
-                </div>
+               
 
             </div>
 
